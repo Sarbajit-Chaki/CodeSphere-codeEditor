87b76c477cf168b7a10b7934e424387f76e9966a
Dibakar Ghosh <ghoshdibakar81@gmail.com>
2024-10-17 13:49:33 +0530

    add mailSender function to send emails

---
+++ backend/utils/mailSender.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+const mainSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+    let info = await transporter.sendMail({
+      from: "CodeSphere",
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
+    });
+
+    return info;
+  } catch (error) {
+    console.log("Error in mailSender: ", error);
+  }
+};
+
+export default mainSender;
