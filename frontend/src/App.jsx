a6fdd580cb2bbbff92b4f1bcfca60d7268080a04
Dibakar Ghosh <ghoshdibakar81@gmail.com>
2024-10-21 19:26:12 +0530

    modified the route for Dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Auth from "./pages/Auth";
 import { ThemeProvider } from "./components/theme-provider";
 import Home from "./pages/Home";
+import DashboardComponent from "./components/Dashboard/DashboardComponent";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
       <div className="bg-black overflow-x-hidden scroll-smooth">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/home" element={<Home />} />
+          <Route element={<Dashboard />}>
+            <Route path="/dashboard" element={<DashboardComponent />} />
+          </Route>
         </Routes>
       </div>
     </ThemeProvider>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,29 +1,17 @@
-import DashLeftComponent from "@/components/Dashboard/DashLeftComponent";
-import RoomHistory from "@/components/Dashboard/RoomHistory";
-import SelectLanguage from "@/components/Dashboard/SelectLanguage";
-import Particles from "@/components/ui/particles"
+import DashboardComponent from "@/components/Dashboard/DashboardComponent";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+
 
 const Dashboard = () => {
-
   return (
-    <div className="relative w-screen bg-[#000000] flex min-h-screen flex-col items-center justify-center rounded-lg border bg-background md:shadow-xl">
-      <Particles
-        className="absolute inset-0"
-        quantity={300}
-        ease={80}
-        color={"#ffffff"}
-        refresh
-      />
-      
-      <div className="flex gap-5">
-        <DashLeftComponent />
-        <div className="flex flex-col gap-y-6">
-        <SelectLanguage />
-        <RoomHistory />
-        </div>
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full overflow-y-auto overflow-x-hidden">
+        <Outlet />
       </div>
     </div>
   );
-}
+};
 
-export default Dashboard
+export default Dashboard;
