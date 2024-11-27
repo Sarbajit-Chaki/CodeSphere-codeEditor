07385f19ec13c91fb59d153033c2e502a7d555b6
Dibakar Ghosh <ghoshdibakar81@gmail.com>
2024-11-27 13:18:18 +0530

    changed the picture in auth page

--- frontend/src/components/Auth/Left.jsx
+++ frontend/src/components/Auth/Left.jsx
@@ -17,7 +17,7 @@ const Left = () => {
         <BlackCircle />
         <OrangeCircle />
         <WhiteCircle />
-        <Card img={img1} className={"  hidden lg:block lg:w-48 2xl:w-48 lg:h-48 2xl:h-48  lg:top-[410px] xl:top-[340px] 2xl:top-[340px] lg:left-[275px] xl:left-[320px] 2xl:left-[320px] delay-[1200ms] transition-all animate-curveMove"} />
+        <Card img={img1} className={"  hidden lg:block lg:w-56 2xl:w-56 lg:h-48 2xl:h-48  lg:top-[410px] xl:top-[340px] 2xl:top-[340px] lg:left-[275px] xl:left-[320px] 2xl:left-[320px] delay-[1200ms] transition-all animate-curveMove"} />
         <Card img={img2} className={"  hidden lg:block lg:w-40 2xl:w-40 lg:h-56 2xl:h-56  lg:top-[480px] xl:top-[400px] 2xl:top-[400px] lg:left-[55px] xl:left-[70px] 2xl:left-[70px] -rotate-[7deg] delay-[2100ms] transition-all animate-curveMove2"} />
         <Text className={" transition-all animate-fadeIn"}/>
       </div>
