2eeb1a08c8864870027449ea0719fdf8d1c81a78
Sarbajit-Chaki <sarbajit.c.0222@inspiria.edu.in>
2024-11-29 19:44:24 +0530

    fix Home page ui scrollToView

--- frontend/src/components/Home/Navbar.jsx
+++ frontend/src/components/Home/Navbar.jsx
@@ -3,6 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
   return (
     <div className=" w-full h-16 flex justify-between items-center bg-[#000814] border-b border-b-slate-700 px-4 sm:px-12 lg:px-36 bg-opacity-50 ">
@@ -12,14 +18,20 @@ const Navbar = () => {
       </div>
 
       <div className=" flex items-center gap-4 font-mono lg:gap-8">
-        <div className=" text-gray-400 hover:text-gray-200 cursor-pointer hidden md:block">
-          <a href="#home">Home</a>
+        <div className=" text-gray-400 hover:text-gray-200 cursor-pointer hidden md:block"
+          onClick={() => scrollToSection("home")}
+        >
+            Home
         </div>
-        <div className=" text-gray-400 hover:text-gray-200 cursor-pointer hidden md:block">
-          <a href="#features">Features</a>
+        <div className=" text-gray-400 hover:text-gray-200 cursor-pointer hidden md:block"
+          onClick={() => scrollToSection("features")}
+        >
+            Features
         </div>
-        <div className=" text-gray-400 hover:text-gray-200 cursor-pointer hidden md:block">
-          <a href="#about">About</a>
+        <div className=" text-gray-400 hover:text-gray-200 cursor-pointer hidden md:block"
+          onClick={() => scrollToSection("about")}
+        >
+            About
         </div>
         <button className="relative flex py-2 px-4 sm:px-6 items-center justify-center overflow-hidden bg-white text-black font-semibold font-mono rounded-full transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-yellow-rich before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56" onClick={() => navigate("/auth")}>
           <span className="relative z-10">Join Now</span>
